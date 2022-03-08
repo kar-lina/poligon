f4252e05574f0075fe0bd9c698ea405f2a37b02d
angelina <akarabutova59@gmail.com>
2022-03-08 10:25:54 +0300

    video 44

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,26 +2,33 @@
 
 namespace App\Providers;
 
+use App\Models\BlogPost;
+use App\Models\BlogCategory;
+use App\Observers\BlogCategoryObserver;
+use App\Observers\BlogPostObserver;
 use Illuminate\Support\ServiceProvider;
+use Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * Bootstrap any application services
+     * 
+     * @return void
+     */
+    public function boot()
+    {
+        Schema::defaultStringLength(191);
+        BlogPost::observe(BlogPostObserver::class);
+        BlogCategory::observe(BlogCategoryObserver::class);
+    }
+
     /**
      * Register any application services.
      *
      * @return void
      */
     public function register()
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
     {
         //
     }
